5f000cd111a11f806a83fd4252b9ff3b52b697e4
R Steiner <rachael.j.steiner@gmail.com>
2022-12-14 18:34:30 -0500

    Add player text parsing to DreidelGame.

--- dreidel.py
+++ dreidel.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 
 import random as rd
+import re
 
 
 class Player:
     
-    def __init__(self, name, pot_size=10):
+    def __init__(self, name, pot_size):
         """Initialize a dreidel player. Give them a name and a pot size,
-        which defaults to 10.
+        which defaults to 10 in the DreidelGame class.
         """
         self.name = name
         self.pot_size = pot_size
+        print(self.name, self.pot_size)
 
 
 class DreidelGame:
 
-    def __init__(self, players):
-        """Initialize a dreidel game, passing a list of player objects."""
-        self.players = players
+    def __init__(self, input_text, default_pot_size=10):
+        """Initialize a dreidel game, starting with a user-given list of
+        players and a default pot size per player of 10 gelt.
+        """
+        # self.players = players
         self.sides = [
             ("nun", "Nun: Nothing! Next!"),
             ("gimel", "Gimel: Gimme all the gelt!"),
@@ -25,6 +29,24 @@ class DreidelGame:
             ("shin", "Shin: Put one in!")
         ]
         self.current_state = None
+        self.input_text = input_text
+        self.default_pot_size = default_pot_size
+        self.parse_input()
+
+    def parse_input(self):
+        """Parse the user-given input into player objects."""
+        split_players = self.input_text.split(sep=",")
+        self.players = [Player(*self.get_pot(player)) for player in split_players]
+
+    def get_pot(self, player_string):
+        pot_regex = re.search("(?<=\()[0-9]+", player_string)
+        if pot_regex:
+            player_name = player_string.split(sep=" (")[0].strip()
+            pot_size = int(pot_regex[0])
+        else:
+            player_name = player_string.strip()
+            pot_size = self.default_pot_size
+        return player_name, pot_size
 
     def spin_dreidel(self):
         """Spin the dreidel! Return a double of the result and its
